35aca994426b3028b33fc4befb85b0e9fc498eae
nirapyus <vjpcanaria@bpsu.edu.ph>
2024-05-19 16:14:03 +0800

    4 pm

--- frozen-ventures/src/pages/admin/components/add-user.jsx
+++ frozen-ventures/src/pages/admin/components/add-user.jsx
@@ -131,7 +131,8 @@ export const AddUser = () => {
 
   return (
     <div className="add-user">
-      <h2>Add New User</h2>
+      <h1>Add New User</h1>
+
       <form onSubmit={handleSubmit}>
         {errors.length > 0 && (
           <div className="error-container">
@@ -147,25 +148,25 @@ export const AddUser = () => {
             <p>{successMessage}</p>
           </div>
         )}
-        <div className="input-field">
-          <label htmlFor="role">Role:</label>
-          <select
-            id="role"
-            name="Role"
-            value={selectedRole}
-            onChange={handleRoleChange}
-          >
-            <option value="" disabled>
-              Select Role
-            </option>
-            <option value="Customer">Customer</option>
-            <option value="Retailer">Retailer</option>
-            <option value="Distributor">Distributor</option>
-            <option value="Manufacturer">Manufacturer</option>
-          </select>
-        </div>
+        <div className="input-container">
+          <div className="input-field">
+            <label htmlFor="role">Role:</label>
+            <select
+              id="role"
+              name="Role"
+              value={selectedRole}
+              onChange={handleRoleChange}
+            >
+              <option value="" disabled>
+                Select Role
+              </option>
+              <option value="Customer">Customer</option>
+              <option value="Retailer">Retailer</option>
+              <option value="Distributor">Distributor</option>
+              <option value="Manufacturer">Manufacturer</option>
+            </select>
+          </div>
 
-        <div className="input-container grid2">
           <div className="input-field">
             <label htmlFor="firstName">First Name:</label>
             <input
@@ -175,6 +176,36 @@ export const AddUser = () => {
               value={inputFName}
               onChange={(e) => setInputFName(e.target.value)}
             />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={inputLName}
+              onChange={(e) => setInputLName(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="input-container">
+          <div className="input-field gender">
+            <label htmlFor="gender">Gender:</label>
+            <select
+              id="gender"
+              name="gender"
+              value={selectedGender}
+              onChange={handleGenderChange}
+            >
+              <option value="" disabled>
+                Select Gender
+              </option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="RatherNotSay">Rather not say</option>
+            </select>
           </div>
 
           <div className="input-field">
@@ -188,6 +219,20 @@ export const AddUser = () => {
             />
           </div>
 
+          <div className="input-field">
+            <label htmlFor="birthdate">Birthdate:</label>
+            <input
+              type="date"
+              id="birthdate"
+              name="birthdate"
+              value={inputBirthdate}
+              onChange={(e) => setInputBirthdate(e.target.value)}
+              max={today}
+            />
+          </div>
+        </div>
+
+        <div className="input-container">
           <div className="input-field">
             <label htmlFor="emailAdd">Email Address:</label>
             <input
@@ -209,57 +254,17 @@ export const AddUser = () => {
               onChange={(e) => setInputPass(e.target.value)}
             />
           </div>
-        </div>
 
-        <div className="input-field">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            value={inputLName}
-            onChange={(e) => setInputLName(e.target.value)}
-          />
-        </div>
-
-        <div className="input-field">
-          <label htmlFor="birthdate">Birthdate:</label>
-          <input
-            type="date"
-            id="birthdate"
-            name="birthdate"
-            value={inputBirthdate}
-            onChange={(e) => setInputBirthdate(e.target.value)}
-            max={today}
-          />
-        </div>
-
-        <div className="input-field gender">
-          <label htmlFor="gender">Gender:</label>
-          <select
-            id="gender"
-            name="gender"
-            value={selectedGender}
-            onChange={handleGenderChange}
-          >
-            <option value="" disabled>
-              Select Gender
-            </option>
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-            <option value="RatherNotSay">Rather not say</option>
-          </select>
-        </div>
-
-        <div className="input-field">
-          <label htmlFor="confirmPass">Confirm Password:</label>
-          <input
-            type="password"
-            id="confirmPass"
-            name="confirmPass"
-            value={inputCPass}
-            onChange={(e) => setInputCPass(e.target.value)}
-          />
+          <div className="input-field">
+            <label htmlFor="confirmPass">Confirm Password:</label>
+            <input
+              type="password"
+              id="confirmPass"
+              name="confirmPass"
+              value={inputCPass}
+              onChange={(e) => setInputCPass(e.target.value)}
+            />
+          </div>
         </div>
 
         {(selectedRole === "Retailer" ||
@@ -277,7 +282,9 @@ export const AddUser = () => {
           </div>
         )}
 
-        <button type="submit">Submit</button>
+        <div className="submit-btn">
+          <button type="submit">Submit</button>
+        </div>
       </form>
     </div>
   );
--- frozen-ventures/src/pages/admin/components/user-list.jsx
+++ frozen-ventures/src/pages/admin/components/user-list.jsx
@@ -8,8 +8,36 @@ import {
 } from "../../../firebase/firebase-admin";
 
 export const UserList = () => {
+  const [inputFName, setInputFName] = useState("");
+  const [inputLName, setInputLName] = useState("");
+  const [inputPass, setInputPass] = useState("");
+  const [inputCPass, setInputCPass] = useState("");
+  const [inputPhone, setInputPhone] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputBirthdate, setInputBirthdate] = useState("");
+  const [selectedRole, setSelectedRole] = useState("");
+  const [selectedGender, setSelectedGender] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const [errors, setErrors] = useState([]);
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const today = new Date().toISOString().split("T")[0];
+
+  const handleRoleChange = (e) => {
+    setSelectedRole(e.target.value);
+  };
+
+  const handleGenderChange = (e) => {
+    setSelectedGender(e.target.value);
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedImage(file);
+  };
   const [userList, setUserList] = useState([]);
-  const [selectedRole, setSelectedRole] = useState("All");
+  const [selectRole, setSelectRole] = useState("All");
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -38,7 +66,85 @@ export const UserList = () => {
     };
 
     fetchUsers();
-  }, [selectedRole]);
+  }, [selectRole]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formErrors = [];
+    setSuccessMessage("");
+
+    if (!selectedRole) {
+      formErrors.push("Role is required");
+    } else if (!inputFName) {
+      formErrors.push("First name is required");
+    } else if (!inputLName) {
+      formErrors.push("Last name is required");
+    } else if (!inputPhone) {
+      formErrors.push("Phone is required");
+    } else if (!inputBirthdate) {
+      formErrors.push("Birthdate is required");
+    } else if (!inputEmail) {
+      formErrors.push("Email is required");
+    } else if (!selectedGender) {
+      formErrors.push("Gender is required");
+    } else if (!inputPass) {
+      formErrors.push("Password is required");
+    } else if (!inputCPass) {
+      formErrors.push("Confirm password is required");
+    } else if (inputPass !== inputCPass) {
+      formErrors.push("Passwords do not match");
+    } else if (!validateContactNumber(inputPhone)) {
+      formErrors.push("Invalid phone number");
+    } else if (!validateEmail(inputEmail)) {
+      formErrors.push("Invalid email address");
+    } else if (!validatePassword(inputPass) || !validatePassword(inputCPass)) {
+      formErrors.push(
+        "Password must include an uppercase letter, symbol, and be at least 6 characters."
+      );
+    } else if (
+      selectedRole === "Retailer" ||
+      selectedRole === "Distributor" ||
+      selectedRole === "Manufacturer"
+    ) {
+      if (!selectedImage) {
+        formErrors.push("Image is required");
+      }
+    }
+
+    if (formErrors.length > 0) {
+      setErrors(formErrors);
+      return;
+    }
+
+    try {
+      const latestUserId = await getLatestUserId();
+      const userId = IdGenerator(latestUserId);
+      await addUserAccountInfo(
+        {
+          inputPass,
+          inputPhone,
+          inputEmail,
+          selectedRole,
+        },
+        userId
+      );
+      await addUserPersonalInfo(
+        {
+          inputFName,
+          inputLName,
+          inputPass,
+          inputBirthdate,
+          selectedGender,
+          selectedImage: "MIYAW",
+        },
+        userId
+      );
+      setSuccessMessage("User added successfully!");
+    } catch (error) {
+      console.error("Error adding user:", error);
+      setErrors(["Error adding user. Please try again."]);
+    }
+  };
 
   return (
     <div className="user-list">
@@ -47,42 +153,43 @@ export const UserList = () => {
       <div className="button-container">
         <button
           className={selectedRole === "All" ? "active" : ""}
-          onClick={() => setSelectedRole("All")}
+          onClick={() => setSelectRole("All")}
         >
           All
         </button>
         <button
           className={selectedRole === "Customer" ? "active" : ""}
-          onClick={() => setSelectedRole("Customer")}
+          onClick={() => setSelectRole("Customer")}
         >
           Customers
         </button>
         <button
           className={selectedRole === "Retailer" ? "active" : ""}
-          onClick={() => setSelectedRole("Retailer")}
+          onClick={() => setSelectRole("Retailer")}
         >
           Retailers
         </button>
         <button
           className={selectedRole === "Distributor" ? "active" : ""}
-          onClick={() => setSelectedRole("Distributor")}
+          onClick={() => setSelectRole("Distributor")}
         >
           Distributors
         </button>
         <button
           className={selectedRole === "Manufacturer" ? "active" : ""}
-          onClick={() => setSelectedRole("Manufacturer")}
+          onClick={() => setSelectRole("Manufacturer")}
         >
           Manufacturers
         </button>
       </div>
-      
+
       <table>
         <thead>
           <tr>
             <th>User ID</th>
             <th>First Name</th>
             <th>Last Name</th>
+            <th>Birthdate</th>
             <th>Gender</th>
             <th>Email</th>
             <th>Phone</th>
@@ -103,6 +210,9 @@ export const UserList = () => {
                 <p>{user.lastName}</p>
               </td>
               <td>
+                <p>{user.birthdate}</p>
+              </td>
+              <td>
                 <p>{user.gender}</p>
               </td>
               <td>
@@ -118,6 +228,172 @@ export const UserList = () => {
           ))}
         </tbody>
       </table>
+
+      <div className="edit-user">
+        <h1>Edit User</h1>
+        <form className="header">
+          <div className="input-field">
+            <label htmlFor="userId">Edit User:</label>
+            <input name="userId" id="userId" type="text" />
+          </div>
+
+          <button>Edit</button>
+        </form>
+
+        <form className="edit-form" onSubmit={handleSubmit}>
+          {errors.length > 0 && (
+            <div className="error-container">
+              {errors.map((error, index) => (
+                <div key={index} className="alert-error">
+                  <p>{error}</p>
+                </div>
+              ))}
+            </div>
+          )}
+          {successMessage && (
+            <div className="success-container">
+              <p>{successMessage}</p>
+            </div>
+          )}
+          <div className="input-container">
+            <div className="input-field">
+              <label htmlFor="role">Role:</label>
+              <select
+                id="role"
+                name="Role"
+                value={selectedRole}
+                onChange={handleRoleChange}
+              >
+                <option value="" disabled>
+                  Select Role
+                </option>
+                <option value="Customer">Customer</option>
+                <option value="Retailer">Retailer</option>
+                <option value="Distributor">Distributor</option>
+                <option value="Manufacturer">Manufacturer</option>
+              </select>
+            </div>
+
+            <div className="input-field">
+              <label htmlFor="firstName">First Name:</label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={inputFName}
+                onChange={(e) => setInputFName(e.target.value)}
+              />
+            </div>
+
+            <div className="input-field">
+              <label htmlFor="lastName">Last Name:</label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={inputLName}
+                onChange={(e) => setInputLName(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="input-container">
+            <div className="input-field gender">
+              <label htmlFor="gender">Gender:</label>
+              <select
+                id="gender"
+                name="gender"
+                value={selectedGender}
+                onChange={handleGenderChange}
+              >
+                <option value="" disabled>
+                  Select Gender
+                </option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="RatherNotSay">Rather not say</option>
+              </select>
+            </div>
+
+            <div className="input-field">
+              <label htmlFor="phoneNum">Phone Number:</label>
+              <input
+                type="number"
+                id="phoneNum"
+                name="phoneNum"
+                value={inputPhone}
+                onChange={(e) => setInputPhone(e.target.value)}
+              />
+            </div>
+
+            <div className="input-field">
+              <label htmlFor="birthdate">Birthdate:</label>
+              <input
+                type="date"
+                id="birthdate"
+                name="birthdate"
+                value={inputBirthdate}
+                onChange={(e) => setInputBirthdate(e.target.value)}
+                max={today}
+              />
+            </div>
+          </div>
+
+          <div className="input-container">
+            <div className="input-field">
+              <label htmlFor="emailAdd">Email Address:</label>
+              <input
+                type="text"
+                id="emailAdd"
+                name="emailAdd"
+                value={inputEmail}
+                onChange={(e) => setInputEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="input-field">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={inputPass}
+                onChange={(e) => setInputPass(e.target.value)}
+              />
+            </div>
+
+            <div className="input-field">
+              <label htmlFor="confirmPass">Confirm Password:</label>
+              <input
+                type="password"
+                id="confirmPass"
+                name="confirmPass"
+                value={inputCPass}
+                onChange={(e) => setInputCPass(e.target.value)}
+              />
+            </div>
+          </div>
+
+          {(selectedRole === "Retailer" ||
+            selectedRole === "Distributor" ||
+            selectedRole === "Manufacturer") && (
+            <div className="input-field image-upload">
+              <label htmlFor="imageUpload">Choose Image:</label>
+              <input
+                type="file"
+                id="imageUpload"
+                name="imageUpload"
+                accept=".jpg, .jpeg, .png"
+                onChange={handleImageChange}
+              />
+            </div>
+          )}
+
+          <div className="submit-btn">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
